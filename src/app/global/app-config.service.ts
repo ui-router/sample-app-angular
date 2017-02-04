fdeec6361560508384921d82c8442a7ae23f12a4
Chris Thielen <github@sandgnat.com>
2017-02-04 12:36:06 -0600

    Implement sort observable in AppConfigService

--- src/app/global/app-config.service.ts
+++ src/app/global/app-config.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/Rx';
 
 /**
  * This service stores and retrieves user preferences in session storage
  */
 @Injectable()
 export class AppConfigService {
-  sort = '+date';
+  private _sort = '+date';
+  sort$ = new BehaviorSubject(this.parseSort(this.sort));
+  get sort() { return this._sort; }
+  set sort(val: string) {
+    this._sort = val;
+    this.sort$.next(this.parseSort(val));
+  }
+
   emailAddress: string = undefined;
   restDelay = 100;
 
   constructor() {
     this.load();
+  }
+
+  parseSort(sort: string) {
+    const defaultSort = '+date';
+    const sortOrder = sort || defaultSort;
+    const pattern = /^([+-])(.*)$/;
+    const match = pattern.exec(sortOrder) || pattern.exec(defaultSort);
+    const [__, order, sortBy] = match;
+
+    return { sortBy, order: (order === '-' ? -1 : 1) };
   }
 
   load() {
@@ -24,5 +42,6 @@ export class AppConfigService {
   save() {
     sessionStorage.setItem('appConfig', JSON.stringify(Object.assign({}, this)));
   }
+
 }
 
