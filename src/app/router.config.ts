eb39119a7db9883697d65e33417a402d46fc228d
Chris Thielen <github@sandgnat.com>
2017-02-05 09:57:29 -0600

    Add ui-router-visualizer

--- src/app/router.config.ts
+++ src/app/router.config.ts
@@ -1,9 +1,13 @@
-import { UIRouter } from 'ui-router-core';
+import { UIRouter, Category } from 'ui-router-core';
 import { googleAnalyticsHook } from './util/ga';
 import { requiresAuthHook } from './global/auth.hook';
+import { Visualizer } from 'ui-router-visualizer';
 
 export function routerConfigFn(router: UIRouter) {
   const transitionService = router.transitionService;
   requiresAuthHook(transitionService);
   googleAnalyticsHook(transitionService);
+
+  router.trace.enable(Category.TRANSITION);
+  router.plugin(Visualizer);
 }
