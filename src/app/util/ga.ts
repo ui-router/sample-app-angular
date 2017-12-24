8b1863ee417635b03455166fa3ea2e587b4b9664
Chris Thielen <cthielen@netflix.com>
2017-12-24 10:47:57 -0800

    refactor(ga): reformat code to closer match official

--- src/app/util/ga.ts
+++ src/app/util/ga.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 
 import { TransitionService } from '@uirouter/core';
-(<any>(function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*(<any>new Date());a=s.createElement(o),
+(<any>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*(<any>new Date());a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 }))(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
