4d87cbb9cf17353669d8e746f1fa95d2bdf178b2
Rolando <rolando@everymundo.com>
2024-08-30 14:44:48 -0400

    Created a Main component to use as the application bootstrap component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { MainComponent } from './main.component';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome.component';
 import { LoginComponent } from './login.component';
 import { HomeComponent } from './home.component';
-import { UIRouterModule, UIView } from '@uirouter/angular';
+import { UIRouterModule } from '@uirouter/angular';
 import { APP_STATES } from './app.states';
 import { GlobalModule } from './global/global.module';
 import { routerConfigFn } from './router.config';
 
 @NgModule({
   declarations: [
+    MainComponent,
     AppComponent,
     WelcomeComponent,
     LoginComponent,
@@ -29,6 +31,6 @@ import { routerConfigFn } from './router.config';
     BrowserModule,
     FormsModule
   ],
-  bootstrap: [UIView]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
